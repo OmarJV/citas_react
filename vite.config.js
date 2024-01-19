a11ad701082ac77428bf9f15cb8de5c3c62fd2cb
OmarJV <omqrjimenez6@hotmail.com>
2024-01-18 20:14:58 -0600

    domain changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://OmarJV.github.io/Citas_React/",
+  base:"https://omarjv.github.io/Citas_React/",
 })
