f2c284f44fc696c4538d013d3ee169c39bbe5397
OmarJV <omqrjimenez6@hotmail.com>
2024-01-23 10:19:50 -0600

    temporal

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   //  <React.StrictMode>
     <HashRouter>
-      <App />
+        <App />
     </HashRouter>
   //  </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   // Que archivos llevaran taildwind o css
-  content: ["./index.html", "./src/**/*.jsx"],
+  content: ["index.html", "./src/**/*.jsx"],
   theme: {
     extend: {
       colors: {
