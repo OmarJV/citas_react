12330df987bb3e77b216eb5092b4f699db0c8c2e
OmarJV <omqrjimenez6@hotmail.com>
2024-01-23 13:49:42 -0600

    Adding hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,51 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './paginas/Home.jsx'
+import Cita from './paginas/Cita.jsx'
+import Contacto from './paginas/Contacto.jsx'
+
+const router = createHashRouter([
+  {
+    path:"/",
+    element: <Home />
+  },
+  {
+    path:"/contacto",
+    element: <Contacto />
+  },
+  {
+    path:"/cita",
+    element: <Cita />
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
+   <React.StrictMode>
+    <RouterProvider router={router}>
+
+        <App />
+    </RouterProvider>
+   </React.StrictMode>,
+
+
   //  <React.StrictMode>
     // <HashRouter>
     //     <App />
     // </HashRouter>
   //  </React.StrictMode>,
 
-  <BrowserRouter>
-      <Routes>
-        <Route path="*" element={ <App /> }>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  // <BrowserRouter>
+  //     <Routes>
+  //       <Route path="*" element={ 
+         
+  //           <App /> 
+  //       }>
+  //       </Route>
+  //     </Routes>
+  //   </BrowserRouter>
 )
